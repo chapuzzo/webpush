4f87fc4c3b3e449b617774ca09dd2d3ab349a174
chapuzzo <luismax@gmail.com>
2025-02-05 23:02:16 +0100

    Update 0001_create_subscriptions.sql

--- migrations/0001_create_subscriptions.sql
+++ migrations/0001_create_subscriptions.sql
@@ -1,1 +1,7 @@
 -- Add migration script here
+CREATE TABLE IF NOT EXISTS subscriptions (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    endpoint TEXT NOT NULL,
+    keys JSON NOT NULL,
+    user_id TEXT
+);
